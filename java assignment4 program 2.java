a1d3085591317caed70a33f012cf10e4df15ec55
21ce003 <110095740+21ce003@users.noreply.github.com>
2022-09-21 15:21:52 +0530

    Add files via upload

---
+++ java assignment4 program 2.java
@@ -0,0 +1,25 @@
+class part2
+{
+  static void check() throws ArithmeticException
+  {  
+    System.out.println("my name is HARIL");
+    throw new ArithmeticException("hi");
+  }
+
+  public static void main(String args[])
+  //here we know that check function is going to throw arithmatic exception so we handle it earliar by throws
+  { 
+    try
+    {
+      check();
+    }
+    catch(ArithmeticException e)
+    {
+        System.out.println("hello");
+    //   System.out.println("hello" +e);
+    //   throw e;
+    }
+  }
+}
+
+
